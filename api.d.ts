32606dd612ce2acbb865bc11095dec3140054907
Soel <shachar.soel@sap.com>
2018-04-05 20:20:20 +0300

    * convert to pure ES5 . * Use eslint to  verify ES5 * Update prettier config.

--- api.d.ts
+++ api.d.ts
@@ -1,78 +1,78 @@
-export as namespace regexpToAst;
+export as namespace regexpToAst
 
 export class RegExpParser {
-  pattern: (input: string) => RegExpAst;
+    pattern: (input: string) => RegExpAst
 }
 
 export interface RegExpAst {
-  type: "Pattern";
-  flags: RegExpFlags;
-  value: Disjunction;
+    type: "Pattern"
+    flags: RegExpFlags
+    value: Disjunction
 }
 
 export interface RegExpFlags {
-  global: boolean;
-  ignoreCase: boolean;
-  multiLine: boolean;
-  unicode: boolean;
-  sticky: boolean;
+    global: boolean
+    ignoreCase: boolean
+    multiLine: boolean
+    unicode: boolean
+    sticky: boolean
 }
 
 export interface Disjunction {
-  type: "Disjunction";
-  value: Alternative[];
+    type: "Disjunction"
+    value: Alternative[]
 }
 
 export interface Alternative {
-  type: "Alternative";
-  value: Term[];
+    type: "Alternative"
+    value: Term[]
 }
 
-export type Term = Atom | Assertion;
+export type Term = Atom | Assertion
 
 export interface Assertion {
-  type:
-    | "StartAnchor"
-    | "EndAnchor"
-    | "WordBoundary"
-    | "NonWordBoundary"
-    | "Lookahead"
-    | "NegativeLookahead";
+    type:
+        | "StartAnchor"
+        | "EndAnchor"
+        | "WordBoundary"
+        | "NonWordBoundary"
+        | "Lookahead"
+        | "NegativeLookahead"
 
-  value?: Disjunction;
+    value?: Disjunction
 }
 
-export type Atom = Character | Set | Group | GroupBackReference;
+export type Atom = Character | Set | Group | GroupBackReference
 
 export interface Character {
-  type: "Character";
-  value: number;
-  quantifier?: Quantifier;
+    type: "Character"
+    value: number
+    quantifier?: Quantifier
 }
 
 export interface Set {
-  type: "Set";
-  value: number[];
-  quantifier?: Quantifier;
+    type: "Set"
+    value: number[]
+    quantifier?: Quantifier
 }
 
 export interface Group {
-  type: "Group";
-  value: Disjunction;
-  capturing: boolean;
-  idx?: number;
-  quantifier?: Quantifier;
+    type: "Group"
+    value: Disjunction
+    capturing: boolean
+    idx?: number
+    quantifier?: Quantifier
 }
 
 export interface GroupBackReference {
-  type: "GroupBackReference";
-  value: number;
-  quantifier?: Quantifier;
+    type: "GroupBackReference"
+    value: number
+    quantifier?: Quantifier
 }
 
 export interface Quantifier {
-  type: "Quantifier";
-  atLeast: number;
-  atMost: number;
-  greedy: boolean;
+    type: "Quantifier"
+    atLeast: number
+    atMost: number
+    greedy: boolean
 }
