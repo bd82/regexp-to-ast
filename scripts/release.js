57c5ec22ce2ddd5fdb971be4c15b3a59bfc37410
Soel <shachar.soel@sap.com>
2018-04-06 17:36:23 +0300

    Release Scripts.

---
+++ scripts/release.js
@@ -0,0 +1,68 @@
+"use strict"
+
+const config = require("./release_config")
+const git = require("gitty")
+const _ = require("lodash")
+const semver = require("semver")
+const jf = require("jsonfile")
+const fs = require("fs")
+
+const myRepo = git("")
+const status = myRepo.statusSync()
+
+// Checks and Validations
+if (
+    !_.isEmpty(status.staged) ||
+    !_.isEmpty(status.unstaged) ||
+    !_.isEmpty(status.untracked)
+) {
+    console.log(
+        "Error: git working directory must be clean in order to perform a release"
+    )
+    process.exit(-1)
+}
+
+const branchesInfo = myRepo.getBranchesSync()
+
+if (branchesInfo.current !== "master") {
+    console.log(
+        "Error: can only perform release job from master or temp_master branch"
+    )
+    process.exit(-1)
+}
+
+const dateTemplateRegExp = /^(## X\.Y\.Z )\(INSERT_DATE_HERE\)/
+if (!dateTemplateRegExp.test(config.changeLogString)) {
+    console.log(
+        "CHANGELOG.md must have first line in the format '## X.Y.Z (INSERT_DATE_HERE)'"
+    )
+    process.exit(-1)
+}
+
+// bump package.json
+const newVersion = semver.inc(config.currVersion, config.mode)
+const bumpedPkgJson = _.clone(config.pkgJson)
+bumpedPkgJson.version = newVersion
+jf.writeFileSync(config.packagePath, bumpedPkgJson, { spaces: 2, EOL: "\r\n" })
+
+// update CHANGELOG.md date
+const nowDate = new Date()
+const nowDateString = nowDate.toLocaleDateString().replace(/\//g, "-")
+const changeLogDate = config.changeLogString.replace(
+    dateTemplateRegExp,
+    `## ${newVersion} (${nowDateString})`
+)
+fs.writeFileSync(config.changeLogPath, changeLogDate)
+
+// Create commit and push to master
+const newTagName = config.tagPrefix + newVersion
+
+myRepo.addSync([config.packagePath, config.changeLogPath, config.htmlDocsPath])
+myRepo.commitSync(`release ${newVersion}`)
+myRepo.createTagSync(newTagName)
+myRepo.push("origin", "master", () => {
+    console.log("finished push to branch")
+})
+myRepo.push("origin", newTagName, () => {
+    console.log("finished push tag")
+})
---
+++ scripts/release_config.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const fs = require("fs")
+const jf = require("jsonfile")
+const path = require("path")
+const _ = require("lodash")
+
+const packagePath = path.join(__dirname, "../package.json")
+const changeLogPath = path.join(__dirname, "../CHANGELOG.md")
+
+const pkgJson = jf.readFileSync(packagePath)
+const changeLogString = fs.readFileSync(changeLogPath, "utf8").toString()
+
+let mode
+if (_.includes(process.argv, "patch")) {
+    mode = "patch"
+} else if (_.includes(process.argv, "minor")) {
+    mode = "minor"
+} else if (_.includes(process.argv, "major")) {
+    mode = "major"
+} else {
+    console.log("release mode (patch|minor|major) not provided")
+    process.exit(-1)
+}
+
+module.exports = {
+    packagePath,
+    changeLogPath,
+    pkgJson,
+    changeLogString,
+    currVersion: pkgJson.version,
+    mode,
+    tagPrefix: "v"
+}
